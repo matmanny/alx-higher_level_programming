8a273f88b76746bee1615173bf25c7d95b796799
matmanny <mannyfekade@gmail.com>
2022-06-09 05:25:29 +0300

    best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    i = 0
+    res = ""
+    if a_dictionary:
+        for p, m in a_dictionary.items():
+            if m > i:
+                res = p
+                i = m
+        return res
+    else:
         return None
-    return max(a_dictionary)
